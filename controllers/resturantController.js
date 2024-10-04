b1ea2ae8a6adaa06b50b64a146520ab60aa0352c
Anand chavan <chavananand959@gmail.com>
2024-10-04 18:06:16 +0530

    create file resturant

--- controllers/resturantController.js
+++ controllers/resturantController.js
@@ -1,5 +1,5 @@
 
-const  t resturatModel = require("../models/resturatModel")
+const   resturatModel = require("../models/resturatModel")
 // Create Restaurant 
 const createResturantController = async(req,res) =>{
 
@@ -25,7 +25,26 @@ try {
                 message:'Please provide title and and address'
             })
         }
-        const newResturant = await resturatModel
+        const newResturant = await resturatModel({
+            title,
+        imageUrl,
+        food,
+        time,
+        pickup,
+        delivery,
+        isOpen,
+        logoUrl,
+        rating,
+        ratingCount,
+        code,
+        coords
+        })
+
+        await newResturant.save()
+        res.status(200).send({
+            success:true,
+            message:"New resturant created"
+        })
     
 } catch (error) {
     console.log(error);
@@ -40,4 +59,65 @@ try {
 
 };
 
-module.exports = {createResturantController}
+// GET All RESTAURANT
+
+ const getAllResturant = async(req,res) =>{
+try {
+
+    const resturant =  await resturatModel.find({})
+    if(!resturant){
+        return res.status(404).send({
+            success:false,
+            message:'No Restaurant Available'
+        })
+    }
+    res.status(200).send({
+        success:true,
+        totalcount:resturant.length,
+        resturant
+    })
+    
+} catch (error) {
+    console.log(error);
+    res.status(500).send({
+        success:false,
+        message:'Error In all  Resturant api',
+    })
+    
+}
+
+
+ };
+
+ // Get Resturant by ID
+ const getResturantByrdController = async(req,res)=>{
+    try {
+
+        const resturantid=req.params.id
+        //find resturant
+        const resturant  = await resturatModel.findById(resturantid)
+        if(!resturant){
+            return res.status(404).send({
+                success:false,
+                message:'No resturant found'
+            })
+        }
+        res.status(200).send({
+            success:true,
+            resturant
+        })
+        
+    } catch (error) {
+        console.log(error);
+    res.status(500).send({
+        success:false,
+        message:'Error In get Resturant  by id api',
+    })
+        
+    }
+
+
+ }
+
+
+module.exports = {createResturantController,getAllResturant,getResturantByidController: getResturantByrdController}
--- routes/resturantRoutes.js
+++ routes/resturantRoutes.js
@@ -1,13 +1,19 @@
 const express = require('express');
 
 const authMiddleware = require('../middleware/authMiddleware');
-const { createResturantController } = require('../controllers/resturantController');
+const { createResturantController, getAllResturant } = require('../controllers/resturantController');
 
 const router = express.Router()
 
 //routes
 //CREATE RESTAURANT || Post 
 router.post('/create',authMiddleware,createResturantController)
+
+// GET ALL RESTURANT || GET
+router.get('/getAll',getAllResturant)
+
+// GET ALL RESTURANT || GET
+router.get('/get/:id')
 
 
 
